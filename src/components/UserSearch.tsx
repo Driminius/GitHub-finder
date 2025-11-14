d3928f213619e833f9b8f1b956f3330ba0a53880
Adrian Do <adrianashimself@yahoo.com>
2025-11-14 09:37:29 -0800

    Fixed UserSearch.tsx error

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -4,7 +4,6 @@ import { fetchGithubUser, searchGithubUser } from "../api/github";
 import UserCard from "./UserCard";
 import RecentSearches from "./RecentSearches";
 import { useDebounce } from "use-debounce";
-import type { GitHubUser } from "../types";
 import SuggestionDropdown from "./SuggestionDropdown";
 
 const UserSearch = () => {
